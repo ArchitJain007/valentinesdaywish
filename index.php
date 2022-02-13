4c8a56875c7228c80f73bfaefb53fc3ab590b033
ArchitJain007 <90677660+ArchitJain007@users.noreply.github.com>
2022-02-13 12:46:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -538,14 +538,23 @@ max-width: 400px;
 </br>
 <center>
 
-<h3><span><font color="white">Add Your Ads here</font></span></h3
+<h3><span><script type="text/javascript">
+	atOptions = {
+		'key' : 'c7c3fcc05160e841b6e39cc7937dbd0b',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://coffeemildness.com/c7c3fcc05160e841b6e39cc7937dbd0b/invoke.js"></scr' + 'ipt>');
+</script></span></h3
 
 </center>
 <br>
 <center>
 <h2><span><font color="white">A Friend is what the heart needs all the time.</font></span></h2>
 <h2><span><font color="white">Loving is not Just Looking at each other, It's Looking in the same Direction.</font></span></h2>
-<a href="https://happyvalentineday.cf"><font color="white">Happy Valentine’s Day!</font></a></span>
+<a href="https://wishvalentinesday.cf"><font color="white"><b>Happy Valentine’s Day!</b></font></a></span>
 
 </center>
 
@@ -570,7 +579,16 @@ max-width: 400px;
 </div>
 <br>
 <center>
-<h3><span><font color="white">Add Your Ads here</font></span>
+<h3><span><script type="text/javascript">
+	atOptions = {
+		'key' : 'c7c3fcc05160e841b6e39cc7937dbd0b',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://coffeemildness.com/c7c3fcc05160e841b6e39cc7937dbd0b/invoke.js"></scr' + 'ipt>');
+</script></span>
     
 </center>
 <br>
@@ -948,7 +966,8 @@ document.oncontextmenu=new Function("alert(message);return false")
 // Anti Adblock Script by https://www.probloggertips.xyz/
   var _0x4e59=['split','253248lFzjJB','remove','hidden','shift','insertBefore','text/javascript','.close','25369fWTUro','parentNode','11wLvilQ','querySelector','async','314264FiyqLS','classList','.tutorial\x20>\x20div','pbt','getElementsByTagName','class','1133687koVsRN','body','211oQwqSt','getAttribute','div','511964rWHMCI','overflow','onerror','149820roGHEi','append','length','1zTOvzf','76874CJYKiZ','1201508bOwHPJ','script','5gbMVAo','437279JtzAiA','add','button','682713FbpvLi','push','7qXwdrl','198139ipIhBA','src','1145snhYTy','1upMcDZ','active','style','preventDefault','https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js','type'];function _0x505a(_0x18d840,_0x1cec87){_0x18d840=_0x18d840-0x171;var _0x4e591f=_0x4e59[_0x18d840];return _0x4e591f;}var _0xa00dd1=_0x505a;(function(_0xb39422,_0x25d9d1){var _0x73d56c=_0x505a;while(!![]){try{var _0x1b40f8=parseInt(_0x73d56c(0x17f))+parseInt(_0x73d56c(0x18f))*parseInt(_0x73d56c(0x179))+parseInt(_0x73d56c(0x177))*parseInt(_0x73d56c(0x182))+parseInt(_0x73d56c(0x184))+-parseInt(_0x73d56c(0x18a))+parseInt(_0x73d56c(0x183))+parseInt(_0x73d56c(0x18c))*-parseInt(_0x73d56c(0x18d));if(_0x1b40f8===_0x25d9d1)break;else _0xb39422['push'](_0xb39422['shift']());}catch(_0x3ceed6){_0xb39422['push'](_0xb39422['shift']());}}}(_0x4e59,0xb3266));var _0x1484=[_0xa00dd1(0x1a1),_0xa00dd1(0x196),_0xa00dd1(0x19d),_0xa00dd1(0x186),_0xa00dd1(0x17e),'<div\x20class=\x22inner\x22>\x20<div\x20class=\x22header\x22>\x20<center><img\x20style=\x22clear:\x20both;\x20text-align:\x20center;\x22\x20border=\x220\x22\x20data-original-height=\x22260\x22\x20data-original-width=\x22260\x22\x20src=\x22https://blogger.googleusercontent.com/img/a/AVvXsEiCI7I39BWvHKT_tqoyLyzKX500-1_0INLC0xWa7gkWwMGDi_2JkDlxQpACTY6UP4-9BoKr6_vTnF59p60yrRoRpvHAXHniBeXleDTywgGLtVz_BIrARp8xZEMpWreWGH70mRvc3ofCESvfK5Ljxd8Q3b2S32N2HfR9K1PBwq3l6vgKoyaFsKptr-p8=w177-h156\x22\x20/></center>\x20<h2>Ad\x20Blocker\x20Detected\x20:(</h2>\x20</div>\x20<div\x20class=\x22isi\x22>\x20<p>Please\x20consider\x20supporting\x20us\x20by\x20disabling\x20your\x20ad\x20blocker.</p><p>Please\x20Disable\x20your\x20adblocker\x20and\x20Refresh\x20the\x20page\x20to\x20view\x20the\x20site\x20content.</p><div></br></div>\x20\x20<div\x20class=\x22pbtadblock\x22></div>\x20</div>\x20</div>\x20</div>',_0xa00dd1(0x174),_0xa00dd1(0x1a2),_0xa00dd1(0x181),_0xa00dd1(0x18e),_0xa00dd1(0x188),'querySelectorAll','classList',_0xa00dd1(0x19b),_0xa00dd1(0x178),'22532LpgvBI',_0xa00dd1(0x192),'adblock','createElement','click',_0xa00dd1(0x197),_0xa00dd1(0x17c),_0xa00dd1(0x190),'innerHTML',_0xa00dd1(0x173),_0xa00dd1(0x17a),_0xa00dd1(0x180),_0xa00dd1(0x171),'1UJEfNY',_0xa00dd1(0x185),_0xa00dd1(0x187),_0xa00dd1(0x19e),_0xa00dd1(0x17d),_0xa00dd1(0x191),_0xa00dd1(0x195),'616480ksPrca',_0xa00dd1(0x19c),_0xa00dd1(0x1a0),_0xa00dd1(0x194)];function _0xb322(_0x360eee,_0x283601){_0x360eee=_0x360eee-0xd0;var _0x1f5f85=_0x1484[_0x360eee];return _0x1f5f85;}(function(_0x12fdb1,_0x557074){var _0x3722dd=_0xa00dd1,_0x43195d=_0xb322;while(!![]){try{var _0x55075c=parseInt(_0x43195d(0xf2))+parseInt(_0x43195d(0xd3))*parseInt(_0x43195d(0xd5))+-parseInt(_0x43195d(0xda))+-parseInt(_0x43195d(0xd2))+-parseInt(_0x43195d(0xd6))*parseInt(_0x43195d(0xe1))+-parseInt(_0x43195d(0xdc))*-parseInt(_0x43195d(0xed))+parseInt(_0x43195d(0xf3))*parseInt(_0x43195d(0xf4));if(_0x55075c===_0x557074)break;else _0x12fdb1[_0x3722dd(0x18b)](_0x12fdb1[_0x3722dd(0x19a)]());}catch(_0x852811){_0x12fdb1[_0x3722dd(0x18b)](_0x12fdb1[_0x3722dd(0x19a)]());}}}(_0x1484,0x5fe32),!function(){var _0x348e45=_0xa00dd1,_0xfadb33=_0xb322;function _0x3412ca(){var _0x189dcd=_0x505a,_0x5a1fb6=_0xb322,_0xfeac31=document[_0x5a1fb6(0xf0)](_0x189dcd(0x17b));_0xfeac31['id']=_0x5a1fb6(0xe4),_0xfeac31[_0x5a1fb6(0xf5)]=_0x5a1fb6(0xe3),document[_0x5a1fb6(0xec)][_0x5a1fb6(0xd1)](_0xfeac31),document[_0x189dcd(0x178)][_0x5a1fb6(0xee)][_0x5a1fb6(0xd7)]=_0x189dcd(0x199);var _0x4978cd=_0xfeac31[_0x5a1fb6(0xe9)](_0x189dcd(0x189));_0xfeac31[_0x5a1fb6(0xde)](_0x5a1fb6(0xe0));var _0x143455=_0xfeac31['querySelectorAll'](_0x5a1fb6(0xf6));for(_0xfeac31=0x0;_0xfeac31<_0x4978cd[_0x5a1fb6(0xe6)];_0xfeac31++)_0x4978cd[_0xfeac31]['addEventListener'](_0x5a1fb6(0xf1),function(_0x732378){var _0x2a1017=_0x189dcd,_0x290da2=_0x5a1fb6;_0x732378[_0x2a1017(0x193)](),_0x732378=this[_0x290da2(0xd0)](_0x2a1017(0x176))[_0x290da2(0xdf)]('\x20')[0x0];for(var _0x40a8d9=0x0;_0x40a8d9<_0x143455[_0x290da2(0xe6)];_0x40a8d9++)_0x143455[_0x40a8d9][_0x290da2(0xea)]['remove'](_0x2a1017(0x191)),_0x4978cd[_0x40a8d9]['classList'][_0x2a1017(0x198)]('active');_0x4978cd[_0x732378-0x1][_0x2a1017(0x172)][_0x290da2(0xe8)](_0x290da2(0xd8)),_0x143455[_0x732378-0x1][_0x290da2(0xea)][_0x290da2(0xe8)](_0x2a1017(0x191));});}var _0x206c7b=document[_0xfadb33(0xf0)](_0xfadb33(0xd4));_0x206c7b[_0xfadb33(0xd9)]=_0xfadb33(0xdb),_0x206c7b[_0xfadb33(0xe5)]=!0x0,_0x206c7b[_0xfadb33(0xe7)]=_0xfadb33(0xdd),_0x206c7b[_0xfadb33(0xe2)]=function(){var _0xcaaee5=_0xfadb33;_0x3412ca(),window[_0xcaaee5(0xef)]=!0x0;};var _0x340314=document[_0x348e45(0x175)](_0x348e45(0x185))[0x0];_0x340314[_0x348e45(0x19f)][_0xfadb33(0xeb)](_0x206c7b,_0x340314);}());
           //]]>
-</script>    
+</script> 
+<script type='text/javascript' src='//coffeemildness.com/e0/ae/54/e0ae54da7f942fa950f498db9744390a.js'></script>
   </body>
 
   <script>
@@ -1063,7 +1082,7 @@ document.oncontextmenu=new Function("alert(message);return false")
 
 <script>
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 10, 2017 00:00:00").getTime();
+var countDownDate = new Date("Feb 14, 2022 00:00:00").getTime();
 
 // Update the count down every 01 second
 var x = setInterval(function() {
